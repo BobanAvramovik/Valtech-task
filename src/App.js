5e8bd2663575cfc49450090b139eb47cdadacb5b
Boban <bobanavramovik@gmail.com>
2022-07-04 16:37:04 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
+import Header from "./components/Header/Header";
+
 function App() {
-  return <div>Hello!</div>;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://picsum.photos/200/300");
+        console.log(response);
+
+        const data = await response.json();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
